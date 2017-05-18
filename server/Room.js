d094b9e6726ffb3cd301d05b355903dcc816a7ee
Miguel Rios <miguelriosoliveira@gmail.com>
2017-05-18 16:59:09 -0300

    WIP: Creating Room class

--- server/Room.js
+++ server/Room.js
@@ -3,4 +3,12 @@ class Room {
         this.player1 = null;
         this.player2 = null;
     }
+
+    addPlayer(player) {
+        if (!this.player1)
+            this.player1 = player;
+        else if (!this.player2)
+            this.player2 = player;
+        else throw "Room is full!";
+    }
 }
